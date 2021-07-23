cf907706e7d5444465fec7db175f7227643d4ca6
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-07-23 20:02:51 -0300

    add accept application/json as response from api

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -30,6 +30,18 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
+        $regras = [
+            'nome' => 'required|unique:marcas',
+            'imagem' => 'required'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute precisa ser preenchido',
+            'nome.unique' => 'O nome da marca já existe'
+        ];
+
+        $request->validate($regras, $feedback);
+
         $marca = $this->marca->create($request->all());
         return response()->json($marca, 201);
     }
