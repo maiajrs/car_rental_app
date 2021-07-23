718c6863c24256834b4e528e5aa22f2e5f52188e
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-07-23 17:10:38 -0300

    refactored the object model marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,8 +18,8 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        $marca = Marca::all();
-        return $marca;
+        $marcas = $this->marca->all();
+        return $marcas;
     }
 
     /**
@@ -26,18 +30,19 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
+        $marca = $this->marca->create($request->all());
         return $marca;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
+        $marca = $this->marca->find($id);
         return $marca;
     }
 
@@ -45,23 +50,25 @@ class MarcaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-       $marca->update($request->all());
-       return $marca;
+        $marca = $this->marca->find($id);
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
+        $marca = $this->marca->find($id);
         $marca->delete();
         return ['msg' => 'A marca foi excluída com sucesso'];
     }
