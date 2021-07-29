51d65ee8962e020f5fbfa19bcd1e96f216cf914b
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-07-29 10:13:21 -0300

    added refresh token method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,8 @@ class AuthController extends Controller
     }
 
     public function refresh() {
-        return 'refresh';
+        $token = auth('api')->refresh();
+        return response()->json(['token' => $token]);
     }
 
     public function me() {
